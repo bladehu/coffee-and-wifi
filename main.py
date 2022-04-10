114348741e45ae221235e15517204a7c1be03a0a
Olivér Süle <sule.oliver91@gmail.com>
2022-04-10 20:17:26 +0200

    Saving keys to environ

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.fields.html5 import URLField, TimeField
 from wtforms.validators import DataRequired
 import csv
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap(app)
 
 CAFE_SYMBOL = "☕"
